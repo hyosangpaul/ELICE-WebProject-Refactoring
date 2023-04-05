c68f86a78f3df464ef8eb21cf8761c8be86b9e8b
paul950104@gmail.com <paul950104@gmail.com>
2023-04-05 19:06:08 +0900

    feat: mysql 연동

--- back/index.js
+++ back/index.js
@@ -1,1 +1,24 @@
-console.log('Hello')
+const express    = require('express');
+const mysql      = require('mysql');
+const dbconfig   = require('./src/config/database');
+const connection = mysql.createConnection(dbconfig);
+
+const app = express();
+
+app.set('port', process.env.PORT || 8000);
+
+app.get('/', (req, res) => {
+  res.send('Root');
+});
+
+app.get('/users', (req, res) => {
+  connection.query('SELECT * from Users', (error, rows) => {
+    if (error) throw error;
+    console.log('User info is: ', rows);
+    res.send(rows);
+  });
+});
+
+app.listen(app.get('port'), () => {
+  console.log('Express server listening on port' + app.get('port'));
+});
---
+++ back/src/config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+    host     : 'localhost',
+    user     : 'root',
+    password : '12341234',
+    database : 'my_db'
+  };
